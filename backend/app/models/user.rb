0aa6a0dac60992930dda49cbeea70e43a21e0581
Kaiter Wu <wukaiter@gmail.com>
2023-03-31 15:08:32 -0400

    setting up migrations for auth

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -10,45 +10,5 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
-  validates :email, 
-    uniqueness: true, 
-    length: { in: 3..255 }, 
-    format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :session_token, presence: true, uniqueness: true
-  validates :password, length: { in: 6..255 }, allow_nil: true
-
-  before_validation :ensure_session_token
-
-  def self.find_by_email(email,password)
-      user = User.find_by(email:email)
-    
-    if user
-      return user if user.authenticate(password)
-    else 
-      return nil 
-    end 
-
-    
-  end 
-
-  def reset_session_token!
-    self.session_token = generate_unique_session_token
-    self.update!({session_token: self.session_token})
-    self.session_token
-  end 
-
-  private
-  def generate_unique_session_token
-    token = SecureRandom::urlsafe_base64 
-        while User.exists?(session_token:token )
-            token = SecureRandom::urlsafe_base64 
-        end 
-        token
-
-  end 
-
-  def ensure_session_token
-    self.session_token ||= generate_unique_session_token
-  end 
+  has_secure_password
 end
-
