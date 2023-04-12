a277eb8fe8860fb889d138738956d109dd9ffd72
Kaiter Wu <wukaiter@gmail.com>
2023-04-12 15:00:59 -0400

    adding dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_one_attached :cover
 
-  has_many :posts
+  has_many :posts , dependent: :destroy
   has_many :friendships , dependent: :destroy
   has_many :friends, through: :friendships
   has_many :comments, dependent: :destroy 
